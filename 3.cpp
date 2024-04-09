61f3b3ea2c01c3287b0e7faadc107d820e22394b
Melu <melacoton11@gmail.com>
2024-04-08 21:39:06 -0300

    el6

---
+++ 3.cpp
@@ -0,0 +1,24 @@
+//Primero hice el ejercicio 6 asi que la explicacion es casi la misma 
+#include <stdio.h>
+#include <string.h>
+char cadena[100];
+int tamanio, i;
+int palindromo = 1; 
+
+int main() {
+    printf("Ingrese una cadena de caracteres: ");
+    scanf("%s", cadena);
+
+    tamanio = strlen(cadena);
+
+    for(i = 0; i < tamanio / 2; i++) {
+        if(cadena[i] != cadena[tamanio - i - 1]) {
+            palindromo = 0; 
+        }
+    }
+
+    if(palindromo)
+        printf("\nEs un palindromo\n");
+    else
+        printf("\nNo es un palindromo\n");
+}
