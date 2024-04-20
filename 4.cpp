5403e15af5abe05f2d3fd86453099ce99ded7a65
Melacoton <145458286+Melacoton@users.noreply.github.com>
2024-04-20 12:24:19 -0300

    Update 4.cpp
    4

--- 4.cpp
+++ 4.cpp
@@ -1,26 +1,26 @@
-//Realiza un programa que reemplace todas las apariciones de un carácter en 
-//una cadena de caracteres ingresada por el usuario por otro carácter también ingresado por el usuario.
+/*4. Realiza un programa que reemplace todas las apariciones de un carácter
+en una cadena de caracteres ingresada por el usuario por otro carácter
+también ingresado por el usuario.*/
 
 #include <stdio.h>
 #include <string.h>
-char cadena [100];
+char cadena[150];
 char caracter, caracter2;
 int i;
-main(){
-	printf("Ingrese la cadena de caracteres: ");
-    scanf("%s", cadena);
-
-    printf("\nIngrese el caracter que desea reemplazar: ");
-    scanf(" %c", &caracter);
-
-    printf("\nIngrese el caracter que lo reemplaza: ");
-    scanf(" %c", &caracter2);
-   
-    for(i=0; i<strlen(cadena); i++){
-    	if (cadena[i]==caracter){
-    		cadena[i]=caracter2;
-		} 
-	}
-	printf("\nLa cadena reemplazada queda: %s", cadena);
+int main(){
+	printf("Ingrese la cadena: ");
+	scanf("%s", & cadena);
 	
+	printf("\nIngrese el caracter que desea reemplazar: ");
+	scanf(" %c", & caracter);
+	
+	printf("\nIngrese el nuevo caracter: ");
+	scanf(" %c", & caracter2);
+	
+	for(i=0;i<strlen(cadena); i++){
+		if(cadena[i]==caracter){
+			cadena[i]=caracter2;
+		}
+	}
+	printf("\nLa nueva cadena es %s", cadena);
 }
