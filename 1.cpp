b9e892a99132b42dc1005a6b3a98114c30dada1d
Melacoton <145458286+Melacoton@users.noreply.github.com>
2024-04-20 12:21:17 -0300

    Update 1.cpp
    1

--- 1.cpp
+++ 1.cpp
@@ -2,10 +2,10 @@
 
 #include <stdio.h>
 #include <string.h>
-char nombre [100];
+char nombre [150];
 main(){
 	printf("Ingrese su nombre: ");
 	scanf("%s", & nombre);
 	
-	printf("hola! %s", nombre);
+	printf("\nHola %s!!", nombre);
 }
