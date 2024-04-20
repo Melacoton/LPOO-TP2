a31f2558887f154e2688e36c20b0121bb971c99b
Melacoton <145458286+Melacoton@users.noreply.github.com>
2024-04-20 12:21:58 -0300

    Update 2.cpp
    2

--- 2.cpp
+++ 2.cpp
@@ -1,19 +1,20 @@
-//Realiza un programa que cuente cuántas vocales tiene una cadena de caracteres ingresada por el usuario.
+/*2. Realiza un programa que cuente cuántas vocales tiene una cadena de
+caracteres ingresada por el usuario.*/
 
 #include <stdio.h>
 #include <string.h>
-char cadena[100];
+char cadena[150];
 int i, cont=0;
 main(){
-	printf("Ingrese una palabra o cadena de caracteres: ");
-	scanf("%s", & cadena[i]);
+	printf("Ingrese la cadena: ");
+	scanf("%s", & cadena);
 	
 	for(i=0; i<strlen(cadena); i++){
-	char caracter =cadena[i];
-    if(caracter == 'a'|| caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u' ||
-    caracter == 'A' || caracter == 'E' ||caracter == 'I' ||caracter == 'O' ||caracter == 'U')
-    {cont++;
-    }
-}
-  printf("\nLa cadena ingresada tiene %d vocales", cont);
+		char caracter= cadena[i];
+		if(caracter == 'a' ||caracter == 'e' ||caracter == 'i' ||caracter == 'o' ||caracter == 'u' ||
+		caracter == 'A' ||caracter == 'E' ||caracter == 'I' ||caracter == 'O' ||caracter == 'U'){
+			cont++;
+		}
+	}
+	printf("\nLa cantidad de vocales es: %d", cont);
 }
