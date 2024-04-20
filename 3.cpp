827835ed489ed5e3ab980bfc560454db000400c3
Melacoton <145458286+Melacoton@users.noreply.github.com>
2024-04-20 12:23:38 -0300

    Update 3.cpp
    3

--- 3.cpp
+++ 3.cpp
@@ -1,21 +1,32 @@
-//Primero hice el ejercicio 6 asi que la explicacion es casi la misma 
+/*3. Realiza un programa que determine si una cadena de caracteres ingresada
+por el usuario es palíndromo (se lee igual de izquierda a derecha que de
+derecha a izquierda).*/
+
 #include <stdio.h>
 #include <string.h>
-char cadena[100];
-int tamanio, i;
-int palindromo = 1; 
-
-int main() {
-    printf("Ingrese una cadena de caracteres: ");
-    scanf("%s", cadena);
-
-    tamanio = strlen(cadena);
-
-    for(i = 0; i < tamanio / 2; i++) {
-        if(cadena[i] != cadena[tamanio - i - 1]) {
-            palindromo = 0; 
-        }
-    }
+char cadena[150];
+int palindromo=1, i;
+int main(){
+	printf("Ingrese la cadena: ");
+	scanf("%s", & cadena);
+	
+	int longitud=strlen(cadena);
+	
+	for(i=0;i<longitud; i++){
+		if(cadena[i]!= cadena[longitud - i - 1]){ /*Esto se utiliza en el programa para comparar 
+cada carácter desde el principio de la cadena (cadena[i]) con su carácter correspondiente
+ desde el final (cadena[tamanio - i - 1]), ejemplo "radar" compara el primer caracter
+  con el ultimo: r=r y asi va determinando si es palindromo o no*/
+  palindromo=0;
+  }
+  }
+  if(palindromo==1){
+  	printf("\nEs palindromo");
+  }
+  else {
+  	printf("\nNo es palindromo");
+  }
+}
 
     if(palindromo)
         printf("\nEs un palindromo");
